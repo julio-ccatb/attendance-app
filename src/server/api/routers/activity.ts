149605aa55ed43d9e55de9c166165c617d21b6ea
Julio Castaño <julio_ccatb@hotmail.com>
2024-09-25 11:07:45 -0400

    Init

---
+++ src/server/api/routers/activity.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { ActivityCreateInputSchema } from "pg/generated/zod";
+
+export const activityRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: protectedProcedure
+    .input(ActivityCreateInputSchema)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.activity.create({ data: input });
+    }),
+
+  getLatest: protectedProcedure.query(async ({ ctx }) => {
+    const post = await ctx.db.activity.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return post ?? null;
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
